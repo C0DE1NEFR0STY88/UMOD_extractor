927d210e9a2af0bf9268a3f8a2eb8d1959f2d322
C0DE1NEFR0STY88 <69596141+C0DE1NEFR0STY88@users.noreply.github.com>
2024-12-27 15:11:04 +0530

    Update umodfile.h

--- umodfile.h
+++ umodfile.h
@@ -1,1 +1,23 @@
+#ifndef UMODFILE_H
+#define UMODFILE_H
 
+#include <QString>
+#include <vector>
+#include <tuple>
+
+class UMODFile 
+{
+public:
+    explicit UMODFile(const QString &filePath);
+    ~UMODFile();
+
+    bool open();
+    std::vector<std::tuple<QString, QString, int>> listFiles();
+    bool extractAll(const QString &outputDir);
+
+private:
+    QString filePath;
+    std::vector<std::tuple<QString, QString, int>> files;
+};
+
+#endif // UMODFILE_H
