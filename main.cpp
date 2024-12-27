08e412eff076e4927c6564dd11d34d8ac81372fe
C0DE1NEFR0STY88 <69596141+C0DE1NEFR0STY88@users.noreply.github.com>
2024-12-27 15:02:54 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,12 @@
+#include <QApplication>
+#include "umodextractor.h"
 
+int main(int argc, char *argv[]) 
+{
+    QApplication app(argc, argv);
+
+    UMODExtractor window;
+    window.show();
+
+    return app.exec();
+}
