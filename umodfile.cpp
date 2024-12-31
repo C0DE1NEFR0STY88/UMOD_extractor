cd753eb5a7a6944824a0a5f73b8f29e066abf5ed
C0DE1NEFR0STY88 <69596141+C0DE1NEFR0STY88@users.noreply.github.com>
2024-12-31 18:23:04 +0530

    Update umodfile.cpp

--- umodfile.cpp
+++ umodfile.cpp
@@ -1,1 +1,30 @@
+#include "umodfile.h"
 
+UMODFile::UMODFile(const QString &filePath) : filePath(filePath) 
+{}
+
+UMODFile::~UMODFile() 
+{}
+
+bool UMODFile::open() 
+{
+    //placeholder logic; NEEDS TO BE UPDATED WITH ACTUAL PARSING.
+    files = 
+            {
+                {"file1.txt", "Scripts", 45},
+                {"texture1.utx", "Textures", 200},
+                {"map1.unr", "Maps", 1024},
+            };
+    return true;
+}
+
+std::vector<std::tuple<QString, QString, int>> UMODFile::listFiles() 
+{
+    return files;
+}
+
+bool UMODFile::extractAll(const QString &outputDir) 
+{
+    //placeholder logic space
+    return true;
+}
